fa27817a1711589cef0a2ff36a0199001e5a0fa2
adamulp <adamgoestoargentina@gmail.com>
2024-03-20 12:56:00 -0300

    Agrega insertionSort
    Actualizaciones después de cursar EDA anoche

---
+++ src/sortingalgos/Ordenamiento.java
@@ -0,0 +1,109 @@
+package sortingalgos;
+
+/**
+ *
+ * @authors:
+ *           Alexis Allendez
+ *           Enzo Casalegno
+ *           Nicolás Hollmann
+ */
+
+/*
+ * Ejercicio 1
+ * Definir una clase ordenamiento que contenga el atributo datos (un array de
+ * enteros) en el cual se agregarán los métodos:
+ * 
+ * Carga(): que llena el arreglo con 10000 números aleatorios.
+ * Muestra()
+ * bubbleSort()
+ * insertionSort()
+ * shellSort()
+ * 
+ * El método main() de la clase principal, tiene que crear un objeto de la clase
+ * ordenamiento e invocar a los métodos y mostrar el arreglo antes y después de
+ * ordenar.
+ */
+public class Ordenamiento {
+    private final static int ARREGLO_TAMANIO = 10000;
+    protected int unsortedNums[];
+    protected int sortedNums[];
+
+    public Ordenamiento(int[] unsortedNums) {
+        this.unsortedNums = unsortedNums;
+        this.sortedNums = null;
+    }
+
+    public Ordenamiento() {
+        this(new int[ARREGLO_TAMANIO]);
+    }
+
+    public void cargar() {
+        int i = 0;
+        for (int num : unsortedNums) {
+            unsortedNums[i] = (int) (Math.random() * 10001);
+            i++;
+        }
+    }
+    
+    public static boolean esVacio(int nums[]){
+        if(nums==null){
+            System.out.println("El arreglo está vacío.");
+            return true;
+        }
+        return false;
+    }
+
+    public static void muestra(int nums[], int anchoPantalla) {
+        if(esVacio(nums)){
+            return;
+        }
+        int i = 0;
+        for (int num : nums) {
+            while (i <= anchoPantalla) {
+                System.out.print(num + ",\t");
+                i++;
+            }
+            i = 0;
+            System.out.print("\n");
+        }
+    }
+
+    public void muestra(int anchoPantalla) {
+        muestra(this.sortedNums, anchoPantalla);
+    }
+    
+    public static void intercambiar(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public static int[] insertionSort(int nums[]){
+        if(esVacio(nums)){
+            return null;
+        }
+        
+        int aux;
+        int j;
+        for(int i=1; i <= (nums.length -1); i++){
+            aux = nums[i];
+            j=i;
+            while(j > 0 && nums[j-1] > aux){
+                intercambiar(nums, j, (j-1));
+                j--;
+            }
+        }
+        return nums;
+    }
+    /*
+     * public static native void arraycopy(Object src, int srcPos,
+     * Object dest, int destPos,
+     * int length);
+     */
+    public void insertionSort() {
+        this.sortedNums = new int[this.unsortedNums.length];
+        System.arraycopy(unsortedNums, 0, sortedNums, 0, unsortedNums.length);
+        sortedNums = insertionSort(unsortedNums);
+    }
+
+}
--- src/sortingalgos/SortingAlgos.java
+++ src/sortingalgos/SortingAlgos.java
@@ -1,22 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sortingalgos;
 
 /**
  *
- * @author adam
+ * @authors:
+ * Alexis Allendez      
+ * Enzo Casalegno       
+ * Nicolás Hollmann     
  */
+
+/*
+Ejercicio 1
+Definir una clase ordenamiento que contenga el atributo datos (un array de
+enteros) en el cual se agregarán los métodos:
+
+    Carga(): que llena el arreglo con 10000 números aleatorios.
+    Muestra()
+    bubbleSort()
+    insertionSort()
+    shellSort()
+
+El método main() de la clase principal, tiene que crear un objeto de la clase
+ordenamiento e invocar a los métodos y mostrar el arreglo antes y después de
+ordenar.
+*/
 public class SortingAlgos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hola mundo");
+        Ordenamiento testCarga = new Ordenamiento();
+        testCarga.cargar();
+//        testCarga.muestra(20);
+        testCarga.insertionSort();
+        testCarga.muestra(20);
+
     }
     
 }
